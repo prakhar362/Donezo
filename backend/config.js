5bf564ad834a85bfa3cbefe27cbdd8f73dacde15
prakhar362 <prakharshri2005@gmail.com>
2025-01-02 10:26:08 +0530

    reset

--- backend/config.js
+++
@@ -1,9 +0,0 @@
-//to avoid backtracking and circular dependencies from passing
-//secret token jwt for both the user and admin from routes dir to middlwares files 
-require('dotenv').config();
-const JWT_SECRET=process.env.JWT_SECRET;
-
-module.exports={
-
-    JWT_SECRET
-};
